d13155dd8c4e9a5d4fe113e0bb9c36397e43eb49
Claudia Pizarro <claudia.cpizarrol@gmail.com>
2024-02-04 18:05:25 -0300

    Activa smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
+
+//Variables para el tooltip
 var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 var tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
+
 //Funcion que permite efecutar el smooth scroll
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+    if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+    scrollTop: $(hash).offset().top
+    }, 800, function(){
+    
+    window.location.hash = hash;
+    });
+    }
+    });
+   });
+   
 
             
 
