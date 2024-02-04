8ce4ad697e6314231f764f6c7306f07218b3cf1f
Claudia Pizarro <claudia.cpizarrol@gmail.com>
2024-02-04 17:11:34 -0300

    Incorpora tooltip en enviar mensaje

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+var tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+//Funcion que permite efecutar el smooth scroll
+
+            
+
+
